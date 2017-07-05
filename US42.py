e2e96cce0e508c094bfe9113e37d27ecba1f83fe
hmigdon <hmigdon@stevens.edu>
2017-07-05 14:54:12 -0400

    Update US42.py

--- US42.py
+++ US42.py
@@ -2,7 +2,7 @@ import BHS_555 as db
 
 def checkDate(dt, idd, context):
     if dt == 'N/A':
-        pass
+        return True
     else:
         try:
             datetime.date(datetime.strptime(dt, "%d %b %Y"))
@@ -15,10 +15,10 @@ def checkDate(dt, idd, context):
 
 def US42():
     for i in IndDic:
-        checkDate(IndDic[i]["BIRT"], IndDic[i][idd],"birth date")
-        checkDate(IndDic[i]["DEAT"], IndDic[i][idd],"death date")
+        checkDate(IndDic[i]["BIRT"], i,"birth date")
+        checkDate(IndDic[i]["DEAT"], i,"death date")
         
         
     for f in FamDic:
-        checkDate(FamDic[f]["MARR"], FamDic[f][ID],"marrige date")
-        checkDate(FamDic[f]["DEAT"], FamDic[f][ID],"divorce date")
+        checkDate(FamDic[f]["MARR"], f,"marrige date")
+        checkDate(FamDic[f]["DEAT"], f,"divorce date")
