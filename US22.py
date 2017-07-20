3444447500ef0630f590b397773bf48fe7b835d0
Bharath Kumar <bkumar@stevens.edu>
2017-07-19 21:03:06 -0400

    Sprint3

--- US22.py
+++ US22.py
@@ -2,7 +2,7 @@ def US22(IndRef):
 	error = []
 	if len(IndRef) != len(set(IndRef)):
 		for p in IndRef:
-			ql = IndRef[0:p]+IndRef[p+1:]
+			ql = IndRef[0:IndRef.index(p)]+IndRef[IndRef.index(p)+1:]
 			for q in ql :
 				if p == q :
 					error.append(q)
